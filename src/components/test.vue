6f1e2e01a6bfb36e3b26473ecf8997f1ef6bf525
Dorou <qinkili@126.com>
2025-04-09 01:00:48 +0800

    v0.0.2

--- src/components/test.vue
+++ src/components/test.vue
@@ -475,71 +475,137 @@
 		return () => clearInterval(intervalId);
 	};
 
-	const createPressureGauge = (element, name, value, unit, center) => {
+	const createPressureGauge = (element) => {
 		const myChart = echarts.init(element.querySelector(".gauge-chart"));
 		const option = {
-			tooltip: {
-				formatter: "{a} <br/>{b} : {c}" + unit,
-			},
 			series: [
 				{
-					name: name,
 					type: "gauge",
-					center: center,
-					radius: "70%",
-					startAngle: 180,
-					endAngle: 0,
-					min: 950,
-					max: 1050,
+					min: 0,
+					max: 100,
 					splitNumber: 10,
+					radius: "80%",
 					axisLine: {
 						lineStyle: {
-							color: [
-								[0.2, "#67e0e3"],
-								[0.8, "#37a2da"],
-								[1, "#fd666d"],
-							],
-							width: 8,
+							color: [[1, "#f00"]],
+							width: 3,
+						},
+					},
+					splitLine: {
+						distance: -18,
+						length: 18,
+						lineStyle: {
+							color: "#f00",
 						},
 					},
 					axisTick: {
-						splitNumber: 5,
-						length: 12,
+						distance: -12,
+						length: 10,
 						lineStyle: {
-							color: "auto",
+							color: "#f00",
 						},
 					},
 					axisLabel: {
-						formatter: function (v) {
-							return v;
+						distance: -50,
+						color: "#f00",
+						fontSize: 25,
+					},
+					anchor: {
+						show: true,
+						size: 20,
+						itemStyle: {
+							borderColor: "#000",
+							borderWidth: 2,
 						},
 					},
 					pointer: {
-						width: 3,
-					},
-					title: {
-						offsetCenter: [0, "-40%"],
-						textStyle: {
-							fontSize: 16,
+						offsetCenter: [0, "10%"],
+						icon: "path://M2090.36389,615.30999 L2090.36389,615.30999 C2091.48372,615.30999 2092.40383,616.194028 2092.44859,617.312956 L2096.90698,728.755929 C2097.05155,732.369577 2094.2393,735.416212 2090.62566,735.56078 C2090.53845,735.564269 2090.45117,735.566014 2090.36389,735.566014 L2090.36389,735.566014 C2086.74736,735.566014 2083.81557,732.63423 2083.81557,729.017692 C2083.81557,728.930412 2083.81732,728.84314 2083.82081,728.755929 L2088.2792,617.312956 C2088.32396,616.194028 2089.24407,615.30999 2090.36389,615.30999 Z",
+						length: "115%",
+						itemStyle: {
+							color: "#000",
 						},
 					},
 					detail: {
-						offsetCenter: [0, "15%"],
-						formatter: "{value}" + unit,
-						textStyle: {
-							fontSize: 20,
-						},
+						valueAnimation: true,
+						precision: 1,
+					},
+					title: {
+						offsetCenter: [0, "-50%"],
 					},
 					data: [
 						{
-							value: value,
-							name: name,
+							value: 58.46,
+							name: "PLP",
 						},
 					],
 				},
+				{
+					type: "gauge",
+					min: 0,
+					max: 60,
+					splitNumber: 6,
+					axisLine: {
+						lineStyle: {
+							color: [[1, "#000"]],
+							width: 3,
+						},
+					},
+					splitLine: {
+						distance: -3,
+						length: 18,
+						lineStyle: {
+							color: "#000",
+						},
+					},
+					axisTick: {
+						distance: 0,
+						length: 10,
+						lineStyle: {
+							color: "#000",
+						},
+					},
+					axisLabel: {
+						distance: 10,
+						fontSize: 25,
+						color: "#000",
+					},
+					pointer: {
+						show: false,
+					},
+					title: {
+						show: false,
+					},
+					anchor: {
+						show: true,
+						size: 14,
+						itemStyle: {
+							color: "#000",
+						},
+					},
+				},
 			],
 		};
+
 		myChart.setOption(option);
+
+		const intervalId = setInterval(() => {
+			myChart.setOption({
+				series: [
+					{
+						type: "gauge",
+						data: [
+							{
+								value: +(Math.random() * 100).toFixed(2),
+								name: "PLP",
+							},
+						],
+					},
+				],
+			});
+		}, 2000);
+
+		return () => clearInterval(intervalId);
 	};
 
 	onMounted(() => {
